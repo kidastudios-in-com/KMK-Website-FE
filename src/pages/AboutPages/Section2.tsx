b307faa03100ed64c38577b4cc77009ff7089830
PranavT <tspranav14@gmail.com>
2023-06-19 15:17:14 +0530

    About Page | 2 Profile per row

--- src/pages/AboutPages/Section2.tsx
+++ src/pages/AboutPages/Section2.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import { Text } from "@nextui-org/react";
 import React from "react";
 
@@ -54,107 +55,210 @@ const Section2 = () => {
 					KamayaKya was born when we saw a major opportunity in the market - the Small-Cap segment. If invested at the right time, these companies can give you massive returns. But how do you know which company will grow?
 					Data-driven Research
 				</Text>
-				<div
-					style={{
-						marginTop: 60,
+				<Box
+					sx={{
+						marginTop: "60px",
 						display: "flex",
 						flexDirection: "row",
 						flexWrap: "wrap",
-						// width: '1300px',
+						width: "100%",
 						maxWidth: "1400px",
 						justifyContent: "center",
-						gap: 30,
+						gap: "25px",
+						"@media only screen and (max-width: 672px)": {
+							gap: "2%",
+						},
 					}}
 				>
-					<div
-						style={{
-							display: "flex",
-							flexDirection: "column",
-							marginBottom: "40px",
-							maxWidth: "300px",
-							alignItems: "center",
-						}}
-					>
-						<img
-							src="/kmk-profile1.jpeg"
-							alt="DhirenS"
-							width="100%"
-							style={{ maxWidth: "800px", height: "400px" }}
-						/>
-						<Text b size={26} css={{ marginTop: 10 }}>
-							Dhiren Shah
-						</Text>
-						<Text b size={18} css={{ lineHeight: 1 }}>
-							Director and Co-Founder
-						</Text>
-					</div>
-					<div
-						style={{
+					<Box
+						sx={{
 							display: "flex",
 							flexDirection: "column",
 							marginBottom: "80px",
 							maxWidth: "300px",
 							alignItems: "center",
+							"@media only screen and (max-width: 672px)": {
+								width: "48%",
+								height: "355px",
+								marginBottom: "20px",
+							},
+						}}
+					>
+						<img
+							src="/kmk-profile1.jpeg"
+							alt="AnandS"
+							width="100%"
+							style={{ height: "400px" }}
+						/>
+						<Text
+							b
+							size={26}
+							css={{
+								marginTop: 10,
+								"@media only screen and (max-width: 672px)": {
+									// lineHeight: 1.5,
+									fontSize: 22,
+								},
+							}}
+						>
+							Dhiren Shah
+						</Text>
+						<Text
+							b
+							size={18}
+							css={{
+								lineHeight: 1,
+								"@media only screen and (max-width: 672px)": {
+									lineHeight: 1.2,
+									fontSize: 16,
+								},
+							}}
+						>
+							Director and Co-Founder
+						</Text>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							marginBottom: "80px",
+							maxWidth: "300px",
+							alignItems: "center",
+							"@media only screen and (max-width: 672px)": {
+								width: "48%",
+								height: "355px",
+								marginBottom: "20px",
+							},
 						}}
 					>
 						<img
 							src="/kmk-profile2.jpeg"
 							alt="AnandS"
 							width="100%"
-							style={{ maxWidth: "800px", height: "400px" }}
+							style={{ height: "400px" }}
 						/>
-						<Text b size={26} css={{ marginTop: 10 }}>
+						<Text
+							b
+							size={26}
+							css={{
+								marginTop: 10,
+								"@media only screen and (max-width: 672px)": {
+									// lineHeight: 1.5,
+									fontSize: 22,
+								},
+							}}
+						>
 							Nitya Shah
 						</Text>
-						<Text b size={18} css={{ lineHeight: 1 }}>
+						<Text
+							b
+							size={18}
+							css={{
+								lineHeight: 1,
+								"@media only screen and (max-width: 672px)": {
+									lineHeight: 1.2,
+									fontSize: 16,
+								},
+							}}
+						>
 							Director and Co-Founder
 						</Text>
-					</div>
-					<div
-						style={{
+					</Box>
+					<Box
+						sx={{
 							display: "flex",
 							flexDirection: "column",
-							marginBottom: "40px",
+							marginBottom: "80px",
 							maxWidth: "300px",
 							alignItems: "center",
+							"@media only screen and (max-width: 672px)": {
+								width: "48%",
+								height: "355px",
+								marginBottom: "20px",
+							},
 						}}
 					>
 						<img
 							src="/kmk-profile3.jpeg"
-							alt="NityaS"
+							alt="AnandS"
 							width="100%"
-							style={{ maxWidth: "800px", height: "400px" }}
+							style={{ height: "400px" }}
 						/>
-						<Text b size={26} css={{ marginTop: 10 }}>
+						<Text
+							b
+							size={26}
+							css={{
+								marginTop: 10,
+								"@media only screen and (max-width: 672px)": {
+									// lineHeight: 1.5,
+									fontSize: 22,
+								},
+							}}
+						>
 							Aniket Kulkarni
 						</Text>
-						<Text b size={18} css={{ lineHeight: 1 }}>
+						<Text
+							b
+							size={18}
+							css={{
+								lineHeight: 1,
+								"@media only screen and (max-width: 672px)": {
+									lineHeight: 1.2,
+									fontSize: 16,
+								},
+							}}
+						>
 							Director and Co-Founder
 						</Text>
-					</div>
-					<div
-						style={{
+					</Box>
+					<Box
+						sx={{
 							display: "flex",
 							flexDirection: "column",
-							marginBottom: "40px",
+							marginBottom: "80px",
 							maxWidth: "300px",
 							alignItems: "center",
+							"@media only screen and (max-width: 672px)": {
+								width: "48%",
+								height: "355px",
+								marginBottom: "20px",
+							},
 						}}
 					>
 						<img
 							src="/kmk-profile4.jpeg"
-							alt="AniketK"
+							alt="AnandS"
 							width="100%"
-							style={{ maxWidth: "800px", height: "400px" }}
+							style={{ height: "400px" }}
 						/>
-						<Text b size={26} css={{ marginTop: 10 }}>
+						<Text
+							b
+							size={26}
+							css={{
+								marginTop: 10,
+								"@media only screen and (max-width: 672px)": {
+									lineHeight: 1,
+									fontSize: 20,
+								},
+							}}
+						>
 							Tushar Raghatate
 						</Text>
-						<Text b size={18} css={{ lineHeight: 1 }}>
+						<Text
+							b
+							size={18}
+							css={{
+								lineHeight: 1.2,
+								"@media only screen and (max-width: 672px)": {
+									lineHeight: 1,
+									fontSize: 16,
+								},
+							}}
+						>
 							Equity Research Analyst
 						</Text>
-					</div>
-				</div>
+					</Box>
+				</Box>
 			</div>
 		</section>
 	);
