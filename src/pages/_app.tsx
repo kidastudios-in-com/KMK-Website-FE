809acac9312609fbdde7e9ea962937da3dcd073b
kidastudios-in-com <account@kidastudios.com>
2023-08-15 02:18:46 +0530

    - integrated analytics (google & hotjar)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { NextUIProvider, createTheme } from "@nextui-org/react";
 import { useSSR } from "@nextui-org/react";
 import { AuthProvider } from "@/components/AuthContext";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { isBrowser } = useSSR();
@@ -46,8 +47,8 @@ function MyApp({ Component, pageProps }: AppProps) {
             {/*<link rel="preload" href="/src/styles/globals.css" as="style" />*/}
             {/*<link rel="stylesheet" href="/src/styles/globals.css" />*/}
 
-            {/* Hotjar */}
-            {/*<script>*/}
+            {/*Hotjar */}
+            {/*<Script>*/}
             {/*  (function(h,o,t,j,a,r){*/}
             {/*  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};*/}
             {/*  h._hjSettings={hjid:3565907,hjsv:6};*/}
@@ -56,14 +57,40 @@ function MyApp({ Component, pageProps }: AppProps) {
             {/*  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;*/}
             {/*  a.appendChild(r);*/}
             {/*})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');*/}
-            {/*</script>*/}
+            {/*</Script>*/}
 
             {/* Google Tag Manager */}
-            <script
-              src="https://www.googletagmanager.com/gtag/js?id=G-PBMR9CBK3J"
-              data-nscript="afterInteractive"
-            ></script>
+            {/*<Script*/}
+            {/*  src="https://www.googletagmanager.com/gtag/js?id=G-PBMR9CBK3J"*/}
+            {/*  data-nscript="afterInteractive"*/}
+            {/*/>*/}
           </Head>
+
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=G-4R949GS408`}
+          />
+
+          <Script id="google-analytics">
+            {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-4R949GS408');
+                `}
+          </Script>
+
+          <Script>
+            {`
+                    (function(h,o,t,j,a,r){
+                      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                      h._hjSettings={hjid:3565907,hjsv:6};
+                      a=o.getElementsByTagName('head')[0];
+                      r=o.createElement('script');r.async=1;
+                      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                      a.appendChild(r);
+                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                `}
+          </Script>
         </AuthProvider>
       </NextUIProvider>
     )
